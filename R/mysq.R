f7b758924684087125710ff90dc574da59ab27c1
Sean Ropp <baconboy596@gmail.com>
2025-01-24 08:42:15 -0600

    Initial files

---
+++ R/mysq.R
@@ -0,0 +1,12 @@
+#' Mysqaure function
+#'
+#' @param x A quantitative vector
+#'
+#' @returns A quantitative vector with sqaured componets
+#' @export
+#'
+#' @examples
+#' mysq(1:10)
+mysq <- function(x){
+  x^2
+}
