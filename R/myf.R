85a53b78d3a3fa70d195a0c1c21ff67847fce1be
Sean Ropp <baconboy596@gmail.com>
2025-02-07 10:23:56 -0600

    changes

--- R/myf.R
+++ R/myf.R
@@ -7,7 +7,8 @@
 #' @returns A quantitative vector
 #' @export
 #'
-#' @examples myf(2,2,c(2,3,4))
+#' @examples
+#' myf(2,2,c(2,3,4))
 #'
 myf = function(x,xk,coef){
   coef[1]+coef[2]*(x) + coef[3]*(x-xk)*(x-xk>0)
