99f6c295a932f0ae28ef7b62b58f91f01edaa0f3
Sean Ropp <baconboy596@gmail.com>
2025-02-17 09:32:05 -0600

    made changes

--- R/myncurve.R
+++ R/myncurve.R
@@ -10,12 +10,14 @@
 #' @examples
 #' myncurve(10,5,6)
 #'
+
 myncurve = function(mu, sigma, a){
-  curve(dnorm(x, mean = mu, sd = sigma), xlim = c(mu - 3 * sigma, mu + 3 * sigma),
+  graphics::curve(stats::dnorm(x, mean = mu, sd = sigma),
+        xlim = c(mu - 3 * sigma, mu + 3 * sigma),
         col = "blue", lwd = 2)
   x_vals <- seq(mu - 3 * sigma, a, length.out = 1000)
-  y_vals <- dnorm(x_vals, mean = mu, sd = sigma)
-  polygon(c(mu - 3 * sigma, x_vals, a), c(0, y_vals, 0), col = rgb(0, 0, 1, 0.5), border = NA)
-  probability <- pnorm(a, mean = mu, sd = sigma)
+  y_vals <- stats::dnorm(x_vals, mean = mu, sd = sigma)
+  graphics::polygon(c(mu - 3 * sigma, x_vals, a), c(0, y_vals, 0), col = grDevices::rgb(0, 0, 1, 0.5), border = NA)
+  probability <- stats::pnorm(a, mean = mu, sd = sigma)
   list(mu = mu, sigma = sigma, a = probability)
 }
