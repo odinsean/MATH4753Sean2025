a0a549c092f90d2c1dc7e6129fba444db6a79739
Sean Ropp <baconboy596@gmail.com>
2025-03-07 09:27:46 -0600

    added fire data

---
+++ R/firedam.R
@@ -0,0 +1,12 @@
+#' fire data set
+#'
+#' A data set that collected distance and damage
+#'
+#' @format A data frame with 15 rows and 2 columns:
+#' \describe{
+#' \item{DISTANCE}{A number}
+#' \item{DAMAGE}{A number}
+#' ...
+#' }
+#' @source \url {https://www.crcpress.com/statistics-for-Engineering-and-the-sciences/Mendenhall-Sinich/p/book/978149872850}
+  "fire"
